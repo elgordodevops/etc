7efe333fd7b0596f6400ddc2b754eeedee12f378
SuperY0 <samaniego715@hotmail.com>
2022-12-28 13:27:32 -0300

    Create clone_folder_structure.bat

---
+++ clone_folder_structure.bat
@@ -0,0 +1,4 @@
+xcopy origen /t /e destino
+
+/t= Copies the subdirectory structure (that is, the tree) only, not files. To copy empty directories, you must include the /e command-line option.
+/e=	Copies all subdirectories, even if they are empty. Use /e with the /s and /t command-line options.
